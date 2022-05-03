10c2b71aaac9323d8ddad132d6eb47691e61d363
Siddharanjan <siddharanjan@gmail.com>
2022-05-03 20:52:16 +1000

    User dao service methods

---
+++ consumers/src/main/java/com/sid/rest/webservice/consumers/user/UserDaoService.java
@@ -0,0 +1,44 @@
+package com.sid.rest.webservice.consumers.user;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class UserDaoService {
+
+    private static List<User> users = new ArrayList<>();
+
+    private static int userCount = 3;
+
+    static {
+        users.add(new User(1, "Sidhiksha", new Date()));
+        users.add(new User(2, "Siddharanjan", new Date()));
+        users.add(new User(3, "Abinaya", new Date()));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User saveUser(User user) {
+        if (user.getId() == null) {
+            user.setId(++userCount);
+        }
+        users.add(user);
+        return user;
+    }
+
+    public User findUser(int id) {
+        for (User user : users) {
+            if (user.getId() == id) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+
+}

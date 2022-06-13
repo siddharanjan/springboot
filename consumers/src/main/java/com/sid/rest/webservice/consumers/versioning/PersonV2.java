ac7184a40f993df71918370b411ac217645955b0
Siddharanjan <siddharanjan@gmail.com>
2022-06-13 11:09:09 +1000

    Changes in files

---
+++ consumers/src/main/java/com/sid/rest/webservice/consumers/versioning/PersonV2.java
@@ -0,0 +1,20 @@
+package com.sid.rest.webservice.consumers.versioning;
+
+public class PersonV2 {
+   private Name name;
+
+   public Name getName() {
+      return name;
+   }
+
+   public void setName(Name name) {
+      this.name = name;
+   }
+
+   public PersonV2(Name name) {
+      this.name = name;
+   }
+
+   public PersonV2() {
+   }
+}
